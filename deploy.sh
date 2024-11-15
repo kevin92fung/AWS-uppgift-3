6c1b1c483f2ab4e14097b20fc084195bc48779e3
kevin <test@test.se>
2024-11-15 10:22:48 +0100

    update

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 # Load environment variables from .env file
-if [ -f .env ]; then
-    export $(grep -v '^#' .env | xargs)
+set -a
+source .env
+set +a
+
+# Validate that required environment variables are set
+if [[ -z "$GITHUB_REPO_OWNER" || -z "$GITHUB_REPO_NAME" || -z "$GITHUB_BRANCH" || -z "$GITHUB_TOKEN" ]]; then
+  echo "Error: Missing required environment variables in .env file."
+  exit 1
 fi
+
 
 # Set variables for the template file and stack name
 TEMPLATE_FILE="Cloudformation.yaml"  # Change this to the path of your CloudFormation template file
@@ -16,4 +23,16 @@ aws cloudformation deploy \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
     FromEmail="$FROM_EMAIL" \
-    ToEmail="$TO_EMAIL"
+    ToEmail="$TO_EMAIL" \
+    GitHubRepoOwner=$GITHUB_REPO_OWNER \
+    GitHubRepoName=$GITHUB_REPO_NAME \
+    GitHubBranch=$GITHUB_BRANCH \
+    GitHubToken=$GITHUB_TOKEN \
+
+# Check if deployment was successful
+if [ $? -eq 0 ]; then
+  echo "CloudFormation stack deployed successfully!"
+else
+  echo "Error: CloudFormation stack deployment failed."
+  exit 1
+fi
